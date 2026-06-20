857cf32675edacf0800e7ed27f39eabbc0e53b5a
Tiago1710 <tiagocorvalan4@gmail.com>
2026-06-20 15:59:11 -0300

    Add files via upload

--- Actividad 2 ARAYS OBJETOS/repuestos.js
+++ Actividad 2 ARAYS OBJETOS/repuestos.js
@@ -1,14 +1,44 @@
+const vehiculos = [
+    {nombre:"Chevrolet Corsa City", precio:39.45},
+    {nombre:"Citroen C4", precio:63},
+    {nombre:"Fiat Palio Weekend", precio:54.4},
+    {nombre:"Fiat Siena", precio:37.2},
+    {nombre:"Ford Explorer XLT 4x4", precio:42.9},
+    {nombre:"Ford Ranger XLT 4x4", precio:66.6},
+    {nombre:"Peugeot 306", precio:25},
+    {nombre:"Renault Laguna", precio:29.5},
+    {nombre:"Suzuki Fun", precio:32.59},
+    {nombre:"Volkswagen Gol", precio:39.8},
+    {nombre:"Volkswagen Suran", precio:13.32}
+];
+
 const presupuestos = [];
 
-function calcularPresupuesto(precio) {
-    const iva = precio * 0.21;
-    const precioContado = precio + iva;
-    const interes = precioContado * 0.10;
-    const precioConInteres = precioContado + interes;
-    const cuota24 = precioConInteres / 24;
-    const cuota36 = precioConInteres / 36;
+const selectVehiculo = document.getElementById("vehiculo");
 
-    return {
+vehiculos.forEach((v, indice) => {
+    selectVehiculo.innerHTML += `
+        <option value="${indice}">
+            ${v.nombre}
+        </option>
+    `;
+});
+
+function calcularPresupuesto(precio){
+
+    let iva = precio * 0.21;
+
+    let precioContado = precio + iva;
+
+    let interes = precioContado * 0.10;
+
+    let precioConInteres = precioContado + interes;
+
+    let cuota24 = precioConInteres / 24;
+
+    let cuota36 = precioConInteres / 36;
+
+    return{
         iva,
         precioContado,
         interes,
@@ -18,66 +48,143 @@ function calcularPresupuesto(precio) {
     };
 }
 
-document.getElementById("formPresupuesto").addEventListener("submit", function(e){
+document
+.getElementById("formPresupuesto")
+.addEventListener("submit", function(e){
+
     e.preventDefault();
 
-    const cliente = document.getElementById("cliente").value;
-    const select = document.getElementById("vehiculo");
+    const nombre =
+    document.getElementById("nombre").value;
 
-    const vehiculo = select.options[select.selectedIndex].text;
-    const precio = parseFloat(select.value);
+    const apellido =
+    document.getElementById("apellido").value;
 
-    const datos = calcularPresupuesto(precio);
+    const indice =
+    document.getElementById("vehiculo").value;
 
-    presupuestos.push({
-        cliente,
-        vehiculo,
-        precio,
-        ...datos
-    });
+    const auto = vehiculos[indice];
 
-    document.getElementById("resultado").innerHTML = `
-        <h3>Presupuesto</h3>
-        Cliente: ${cliente}<br>
-        Vehículo: ${vehiculo}<br>
-        Precio Lista: ${precio}<br>
-        IVA: ${datos.iva.toFixed(2)}<br>
-        Precio Contado: ${datos.precioContado.toFixed(2)}<br>
-        Interés: ${datos.interes.toFixed(2)}<br>
-        Precio con Interés: ${datos.precioConInteres.toFixed(2)}<br>
-        Cuota 24: ${datos.cuota24.toFixed(2)}<br>
-        Cuota 36: ${datos.cuota36.toFixed(2)}
-    `;
+    const calculo =
+    calcularPresupuesto(auto.precio);
+
+    const presupuesto = {
+
+        nombre,
+        apellido,
+        vehiculo:auto.nombre,
+        precio:auto.precio,
+
+        iva:calculo.iva,
+        contado:calculo.precioContado,
+        interes:calculo.interes,
+        final:calculo.precioConInteres,
+        cuota24:calculo.cuota24,
+        cuota36:calculo.cuota36
+    };
+
+    presupuestos.push(presupuesto);
+
+    mostrarTabla();
+
+    this.reset();
+
 });
 
-function buscarCliente(){
-    const nombre = document.getElementById("buscarCliente").value.toLowerCase();
+function mostrarTabla(){
+    const tabla =
+    document.getElementById("tablaPresupuestos");
 
-    const encontrados = presupuestos.filter(
-        p => p.cliente.toLowerCase().includes(nombre)
-    );
+    tabla.innerHTML = "";
 
-    let salida = "<ul>";
+    presupuestos.forEach(p => {
 
-    encontrados.forEach(p => {
-        salida += `<li>${p.cliente}: ${p.vehiculo}</li>`;
+        tabla.innerHTML += `
+        <tr>
+            <td>${p.nombre} ${p.apellido}</td>
+            <td>${p.vehiculo}</td>
+            <td>${p.precio.toFixed(2)}</td>
+            <td>${p.iva.toFixed(2)}</td>
+            <td>${p.contado.toFixed(2)}</td>
+            <td>${p.interes.toFixed(2)}</td>
+            <td>${p.final.toFixed(2)}</td>
+            <td>${p.cuota24.toFixed(2)}</td>
+            <td>${p.cuota36.toFixed(2)}</td>
+        </tr>
+        `;
     });
 
-    salida += "cliente no encontrado</ul>";
-
-    document.getElementById("listaCliente").innerHTML = salida;
 }
 
-function mostrarMayores(){
-    const mayores = presupuestos.filter(p => p.precio > 35);
+function buscarCliente(){
+    const cliente =
+    document
+    .getElementById("buscarCliente")
+    .value
+    .toLowerCase();
+    const encontrados =
+    presupuestos.filter(p =>
+        p.nombre.toLowerCase().includes(cliente) ||
+        p.apellido.toLowerCase().includes(cliente)
+    );
 
-    let salida = "<ul>";
+    let salida = "";
+    if(encontrados.length > 0){
+        salida = "<ul>";
+        encontrados.forEach(p => {
+            salida += `
+            <li>
+                ${p.vehiculo}
+            </li>
+            `;
+        });
+        salida += "</ul>";
+    }else{
+        salida = "Cliente no encontrado";
 
-    mayores.forEach(p => {
-        salida += `<li>${p.vehiculo} - ${p.precio}</li>`;
-    });
+    }
 
-    salida += "</ul>";
+    document
+    .getElementById("resultadoCliente")
+    .innerHTML = salida;
+}
 
-    document.getElementById("listaMayores").innerHTML = salida;
+function buscarPrecio(){
+
+    const precio =
+    parseFloat(
+        document.getElementById("precioFiltro").value
+    );
+
+    const encontrados =
+    vehiculos.filter(v =>
+        v.precio > precio
+    );
+
+    let salida = "";
+
+    if(encontrados.length > 0){
+
+        salida = "<ul>";
+
+        encontrados.forEach(v => {
+
+            salida += `
+            <li>
+                ${v.nombre} - ${v.precio}
+            </li>
+            `;
+        });
+
+        salida += "</ul>";
+
+    }else{
+
+        salida = "No se encontraron vehículos.";
+
+    }
+
+    document
+    .getElementById("resultadoPrecio")
+    .innerHTML = salida;
 }
